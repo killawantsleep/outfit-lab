ab7a3672059ec4c0fc84f87880e34accc9fe2a08
killa <doronindan77749@gmail.com>
2025-03-24 21:54:50 +0300

    12123

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,22 +1,23 @@
-const CACHE_NAME = 'outfitlab-v3';
+const CACHE_NAME = 'outfitlab-v1';
 const ASSETS = [
   '/',
   '/index.html',
   '/styles.css',
   '/app.js',
-  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Playfair+Display:wght@700&display=swap'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
 });
