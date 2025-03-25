ad4f8adb6672c3b3e4ce67528180a4c74e730409
killa <doronindan77749@gmail.com>
2025-03-25 17:55:24 +0300

    zxczx

--- docs/app.js
+++ docs/app.js
@@ -56,7 +56,7 @@ async function loadItems() {
   showLoading(true);
 
   try {
-    const response = await fetch(`${GOOGLE_SCRIPT_URL}?t=${Date.now()}`);
+    const response = await fetch(`${CONFIG.SCRIPT_URL}?t=${Date.now()}`);
     const data = await response.json();
     
     if (!Array.isArray(data)) throw new Error("Invalid data format");
